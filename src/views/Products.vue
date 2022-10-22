70f3c1009aa1580299f5f57d9b51c9d16ecf7de0
mohamad <mohamad.gharib@ut.ee>
2022-10-23 01:27:42 +0300

    1st

---
+++ src/views/Products.vue
@@ -0,0 +1,49 @@
+<template>
+ <div class="products">
+  <h2> Our Products page</h2>
+  <product-compo-one/> 
+  <button v-on:click="IncreasePrice "> Increase price </button>
+  <button v-on:click="DecreasePrice"> Decrease price </button><br/>
+  <product-compo-two/>
+ </div>
+</template>
+
+<script>
+import ProductCompoOne from "@/components/ProductCompoOne.vue";
+import ProductCompoTwo from "@/components/ProductCompoTwo.vue";
+export default {
+name: "Products",
+components: { ProductCompoOne, ProductCompoTwo },
+
+methods: {
+IncreasePrice: function() {
+    /*
+    When using the struct mode in the vuex store, we cannot mutate state outside mutation handlers.
+    Therefore, using the following function will produce an error
+    
+    this.$store.state.productList.forEach(product => {
+        product.price += 1;
+        }) 
+    */
+//this.$store.commit("IncreasePrice") // mutations
+this.$store.dispatch("IncreasePriceAct")
+},
+DecreasePrice: function() {
+// this.$store.commit("DecreasePrice")  // mutations
+this.$store.dispatch("DecreasePriceAct")
+}}
+}
+</script>
+
+<style scoped>
+button{
+background:  rgb(31, 163, 163);
+border: 0;
+padding: 10px 20px 10px 20px;
+margin-top:  20px;
+margin-bottom:  20px;
+margin-right:  10px;
+color: white;
+border-radius: 20px;
+}
+</style>
